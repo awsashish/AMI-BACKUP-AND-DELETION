1fae95dc538710be6c77310f3d827ea820352dad
ashish <ashish.sharma@seynse.com>
2019-03-12 21:40:53 +0530

    Added code for mysel.

---
+++ code.py
@@ -0,0 +1,100 @@
+# way to run the script
+################
+# python2 ami_backup.py ipaddress 
+################
+
+import boto
+import boto.ec2
+import sys
+import time
+import datetime
+import dateutil.parser
+from boto import ec2
+from boto.ec2 import connect_to_region
+from datetime import date, timedelta
+
+img_list = []
+conn = ec2.connect_to_region('us-west-2')
+
+def mail():
+	import smtplib
+	from email.MIMEMultipart import MIMEMultipart
+	from email.MIMEText import MIMEText
+	
+	text = ''
+	for img_name in img_list:
+		text += str(img_name)+'\n'
+	
+	msg = MIMEText(text)
+	fromaddr = "emailaddress@domain.com"
+	toaddr = "awsashish@gmail.com"
+	sub = 'AMI BACKUP MAIL'
+	body = 'ami backup mail'
+	msg['From'] = fromaddr
+	msg['To'] = toaddr
+	msg['Subject'] = sub
+	server = smtplib.SMTP('mail.domain.com',587)
+	server.starttls()
+	server.login(fromaddr, "password")
+	server.sendmail(fromaddr, toaddr, msg.as_string())
+	server.quit()	
+
+
+def ami_creation():
+	try:
+		date = str(time.strftime("%b-%d-%Y"))
+		reservations = conn.get_all_instances()
+		for reservation in reservations:
+			for instances in reservation.instances:
+				ipaddress = instances.ip_address
+                state = instances.state
+				#if ipaddress == ip:
+				if state == 'running':
+					
+					print('\n')
+					print " CREATTING AMI OF ",ipaddress
+					print('\n')
+					ami_name = str(ipaddress)+'-'+date
+					
+					ami_id = instances.create_image(ami_name, description='Created by AMIBackup', no_reboot=True, dry_run=True)
+					success = ' CREATTING AMI OF ',str(ipaddress)
+					img_list.append(success)
+	except Exception as e:
+		print(e)
+		error = 'ERROR WHILE CREATING BACKUP OF ',ip
+		img_list.append(error)
+		img_list.append('\n')
+		img_list.append(e)
+		
+		
+
+
+def ami_deletion():
+	flag = 0
+	images = sorted(connect_to_region('us-west-2').get_all_images(owners='self'))
+	img_list.append('\n')
+	img_list.append('REMOVING FOLLOWING OLD AMI')
+	img_list.append('\n')
+	for img in images:
+		
+		img_date = img.creationDate
+        img_desc = img.description
+		img_date = dateutil.parser.parse(img_date).date()	
+		today = datetime.date.today()
+		differnce = today - img_date
+		if differnce.days >= 10 and img_desc == 'Created by AMIBackup':
+			flag += 1
+			img_list.append(img.name)		
+			print (" Deleting the AMI ",img)
+			
+			img_list.append('\n')
+			print('\n')
+			conn.deregister_image(img.id)
+		
+	if flag == 0:
+		img_list.append("No older backups")
+		print(" No older backups")
+
+ami_creation()
+ami_deletion()
+mail()
